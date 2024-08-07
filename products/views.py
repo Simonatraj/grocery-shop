86a4cefec620819891678000491fd0daa813239c
Simona <trajkovskasimona10@gmail.com>
2024-08-08 01:56:53 +0200

    migrations

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
     return HttpResponse('Hello World')
+
 
 def show(request):
     return HttpResponse('New Product')
